3fd75384be346ed0f99cf716249eb5930428aad5
Md Nouman <noumantannu06@gmail.com>
2026-04-16 14:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// script.js - Continuous Line Rotation
+
+// Global rotation angle variable
+let rotationAngle = 0;
+
+// Select the line element
+const line = document.getElementById('line');
+
+// Use setInterval to update rotation every 20ms
+setInterval(() => {
+    // Increment angle by 2 degrees
+    rotationAngle += 2;
+    
+    // Apply CSS transform rotation
+    line.style.transform = `rotate(${rotationAngle}deg)`;
+}, 20);
